1cd8ebd9b70bfb8914071bb07d5dfbd429ca5eb5
Yung <fairytaledadala@gmail.com>
2021-02-18 15:00:20 +0800

    MathBranch

--- GitBranchSample/src/GitBranchApp/MainApp.java
+++ GitBranchSample/src/GitBranchApp/MainApp.java
@@ -5,6 +5,17 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Version");
+		
+		Math cMath = new Math();
+		System.out.println(cMath.add(1, 2));
 	}
+	
+	
 
+}
+
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
